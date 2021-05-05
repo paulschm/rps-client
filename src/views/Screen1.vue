a0186e123207960596ac7a9eb4b4ace27898ccaa
martingasser <martin.gasser@gmail.com>
2021-05-05 10:32:35 +0200

    finished step 1

---
+++ src/views/Screen1.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Screen 1</h1>
+        <router-link to="/screen2">Screen2</router-link>
+    </div>
+</template>
---
+++ src/views/Screen2.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Screen 2</h1>
+        <router-link to="/screen3">Screen3</router-link>
+    </div>
+</template>
