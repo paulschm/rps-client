c73dac20a671993d1695e40b73f26b2827962886
martingasser <martin.gasser@gmail.com>
2021-05-05 10:44:42 +0200

    basic connection handling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import VueSocketIO from 'vue-socket.io';
+import SocketIO from 'socket.io-client'
+
+const socket = SocketIO('http://localhost:8081', {})
+
+const vueSocket = new VueSocketIO({
+  debug: false,
+  connection: socket
+  // vuex: {
+  //   store,
+  //   actionPrefix: "socket_"
+  // }  
+})
+
+Vue.use(vueSocket)
 
 Vue.config.productionTip = false
 
